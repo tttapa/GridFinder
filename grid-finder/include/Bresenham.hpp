7cca514b9f8642223dbc40ac05fde83757b69246
Pieter <pieter.pas@outlook.com>
2019-03-23 16:09:49 +0100

    Speedup by moving BresenhamLine inline

--- grid-finder/include/Bresenham.hpp
+++ grid-finder/include/Bresenham.hpp
@@ -7,13 +7,53 @@
 
 class BresenhamLine {
   public:
-    BresenhamLine(Pixel start, double angle, size_t w, size_t h);
-    BresenhamLine(Pixel start, int cos, int sin, size_t w, size_t h);
+    BresenhamLine(Pixel start, int cos, int sin, size_t w, size_t h)
+        : px0(start), px(start),  // Starting point
+          dx(cos),
+          dy(sin),  // cosine and sine of the slope of the line (scaled)
+          adx(std::abs(dx)),
+          ady(std::abs(dy)),             // absolute values of cos and sin
+          xinc(sgn(dx)), yinc(sgn(dy)),  // increment steps for x and y
+          steep(ady > adx),  // whether to increment x or y on each iteration
+          w(w), h(h) {       // size of the canvas
+        if (steep)
+            error = (adx - ady) / 2;
+        else
+            error = (ady - adx) / 2;
+    }
+
     BresenhamLine(Pixel start, CosSin angle, size_t w, size_t h)
         : BresenhamLine(start, angle.cos, angle.sin, w, h) {}
 
-    bool hasNext() const;
-    Pixel next();
+    BresenhamLine(Pixel start, double angle, size_t w, size_t h)
+        : BresenhamLine(start, cos(angle), sin(angle), w, h) {}
+
+    bool hasNext() const { return px.inRange(w, h); }
+
+    size_t getCurrentLength() const { return length; }
+
+    Pixel next() {
+        if (!hasNext())
+            throw std::out_of_range("No more pixels on line withing canvas!");
+        Pixel result = px;
+        if (steep) {
+            if (error >= 0) {
+                px.x += xinc;
+                error -= 2 * ady;
+            }
+            px.y += yinc;
+            error += 2 * adx;
+        } else {
+            if (error >= 0) {
+                px.y += yinc;
+                error -= 2 * adx;
+            }
+            px.x += xinc;
+            error += 2 * ady;
+        }
+        ++length;
+        return result;
+    }
 
     static int cos(double angle) {
         return round(errorScalingFactor * std::cos(angle));
@@ -22,8 +62,6 @@ class BresenhamLine {
     static int sin(double angle) {
         return round(errorScalingFactor * std::sin(angle));
     }
-
-    size_t getCurrentLength() const;
 
   private:
     const static int errorScalingFactor = std::numeric_limits<int>::max() / 2;
@@ -36,4 +74,9 @@ class BresenhamLine {
     size_t w, h;
     int error;
     size_t length = 0;
+
+    template <typename T>
+    static constexpr int sgn(T val) {
+        return (T(0) < val) - (val < T(0));
+    }
 };
